26188e293b572a496c5368893d64f77547c8d9d7
unknown <itspdrajan@gmail.com>
2024-05-16 15:38:13 +0530

    added profile

--- fams/urls.py
+++ fams/urls.py
@@ -4,10 +4,9 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 
-
 app_name = "fams"
 
 urlpatterns = [
-
-
+    path('faculty_list/', faculty_list, name="faculty list"),
+    path('profile', faculty_profile, name="faculty profile"),
 ]
--- fams/views.py
+++ fams/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def faculty_list(request):
+    return render(request, 'home/faculty_list.html',)
+
+
+def faculty_profile(request):
+    return render(request, 'faculty/profile.html')
