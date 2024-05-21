3912f90daf9940326388a615445b774b2bcb2e2a
sid2983 <sid24000576@gmail.com>
2024-05-21 23:29:55 +0530

    edit views

--- fams/urls.py
+++ fams/urls.py
@@ -16,6 +16,15 @@ urlpatterns = [
     path('add_research_project/', add_research_project, name='add_research_project'),
     path('add_patent/', add_patent, name='add_patent'),
     path('add_committee_membership/',add_committee_membership, name='add_committee_membership'),
+    path('add_publication/', add_publication, name='add_publication'),
     path('dashboard/', dashboard, name='dashboard'),
     path('batch_list/', batch_list, name='batch_list'),
+    path('edit_experience/<int:pk>/', edit_experience, name='edit_experience'),
+    path('edit_education/<int:pk>/', edit_education, name='edit_education'),
+    path('edit_honor/<int:pk>/', edit_honor, name='edit_honor'),
+    path('edit_doctoral_thesis/<int:pk>/', edit_doctoral_thesis, name='edit_doctoral_thesis'),
+    path('edit_committee_membership/<int:pk>/', edit_committee_membership, name='edit_committee_membership'),
+    path('edit_research_projects/<int:pk>/', edit_research_projects, name='edit_research_projects'),
+    path('edit_patent/<int:pk>/', edit_patent, name='edit_patent'),
+    
 ]
