f4e2869f12598eae43ea72a2c85ce843e1537a2e
sid2983 <sid24000576@gmail.com>
2024-05-22 22:53:04 +0530

    data added

---
+++ import_students.py
@@ -0,0 +1,85 @@
+import os
+import django
+import csv
+import datetime
+
+# Setup Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sams.settings')
+django.setup()
+
+from users.models import CustomUser, Student, Batch,Faculty
+from django.contrib.auth.models import Group
+from django.shortcuts import get_object_or_404
+
+def current_year():
+    return datetime.date.today().year
+
+def year_choices():
+    return [(r, r) for r in range(1980, current_year() + 1)]
+
+def import_students(csv_file):
+    try:
+        with open(csv_file, newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            student_group, created = Group.objects.get_or_create(name='student')
+
+            faculty = get_object_or_404(Faculty, id=3)# for rhythm mam batch
+            print(faculty)
+
+            for row in reader:
+                enrollment_number = row['Enrolment No']
+                full_name = row['Student Name']
+                first_name, last_name = full_name.split(' ', 1) if ' ' in full_name else (full_name, '')
+                # print(first_name,last_name,)
+
+                user, created = CustomUser.objects.get_or_create(
+                    username=f"{first_name}{enrollment_number[:3]}",
+                    defaults={
+                        'first_name': first_name,
+                        'last_name': last_name,
+                        'password': f"{first_name[0]}{enrollment_number}",  # you may want to set a default password
+                    }
+                )
+                print(f"user {user} created successfully ")
+                if created:
+                    user.groups.add(student_group)
+                    print(f"{user} added to {student_group} successfully")
+
+
+                # Assuming you have a way to determine batch_year, semester, section, shift
+                batch_year = 2024  # Replace with your logic
+                semester = 6       # Replace with your logic
+                section = 'A'      # Replace with your logic
+                shift = 'Morning'  # Replace with your logic
+
+                batch, created = Batch.objects.get_or_create(
+                    year=batch_year,
+                    section=section,
+                    shift=1,  # Replace with appropriate logic to get the correct shift
+                    course='bca',  # Replace with appropriate logic to get the correct course
+                    assigned_to=faculty
+                )
+
+                print(f"Batch {batch} Created successfully")
+
+                student, created = Student.objects.get_or_create(
+                    user=user,
+                    defaults={
+                        'enrollment_number': enrollment_number,
+                        'batch': batch,
+                        'batch_year': batch_year,
+                        'semester': semester,
+                        'section': section,
+                        'shift': shift,
+                        'about':"Student of Maharaja Surajmal Institute"
+                    }
+                )
+
+                print(f'Successfully processed {student} with  {enrollment_number}')
+
+    except FileNotFoundError:
+        print(f'File "{csv_file}" does not exist')
+
+if __name__ == "__main__":
+    csv_file = 'student_list1.csv'  # Path to your CSV file
+    import_students(csv_file)
